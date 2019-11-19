8de31b0059d4cad50b87b6f5ce41d4f088885145
mercedesveselka <57720309+mercedesveselka@users.noreply.github.com>
2019-11-18 20:26:13 -0500

    Function Uppercase
    Made lowercase letters into uppercase letters.

---
+++ Assignment Submissions/Homework 6 Assignment.py
@@ -0,0 +1,143 @@
+#Print the uppercase equivalent of every lowercase letter
+#Take a lowercase letter
+#Find the unicode for this letter.
+#Find the unicode for the uppercase version
+#while the unicode for the lowercase is one thing, change it to the unicode for uppercase
+#transform unicode back into the character
+
+print("Give me a lowercase letter.")
+Input = ord(input())
+vara = ord('a')
+varA = ord('A')
+varb = ord('b')
+varB = ord('B')
+varc = ord('c')
+varC = ord('C')
+vard = ord('d')
+varD = ord('D')
+vare = ord('e')
+varE = ord('E')
+varf = ord('f')
+varF = ord('F')
+varg = ord('g')
+varG = ord('G')
+varh = ord('h')
+varH = ord('H')
+vari = ord('i')
+varI = ord('I')
+varj = ord('j')
+varJ = ord('J')
+vark = ord('k')
+varK = ord('K')
+varl = ord('l')
+varL = ord('L')
+varm = ord('m')
+varM = ord('M')
+varn = ord('n')
+varN = ord('N')
+varo = ord('o')
+varO = ord('O')
+varp = ord('p')
+varP = ord('P')
+varq = ord('q')
+varQ = ord('Q')
+varr = ord('r')
+varR = ord('R')
+varst = ord('s')
+varS = ord('S')
+vart = ord('t')
+varT = ord('T')
+varu = ord('u')
+varU = ord('U')
+varv = ord('v')
+varV = ord('V')
+varw = ord('w')
+varW = ord('W')
+varx = ord('x')
+varX = ord('X')
+vary = ord('y')
+varY = ord('Y')
+varz = ord('z')
+varZ = ord('Z')
+
+for i in range(1):
+    while Input == vara:
+        new = chr(varA)
+        break
+    while Input == varb:
+        new = chr(varB)
+        break
+    while Input == varc:
+        new = chr(varC)
+        break
+    while Input == vard:
+        new = chr(varD)
+        break
+    while Input == vare:
+        new = chr(varE)
+        break
+    while Input == varf:
+        new = chr(varF)
+        break
+    while Input == varg:
+        new = chr(varG)
+        break
+    while Input == varh:
+        new = chr(varH)
+        break
+    while Input == vari:
+        new = chr(varI)
+        break
+    while Input == varj:
+        new = chr(varJ)
+        break
+    while Input == vark:
+        new = chr(varK)
+        break
+    while Input == varl:
+        new = chr(varL)
+        break
+    while Input == varm:
+        new = chr(varM)
+        break
+    while Input == varn:
+        new = chr(varN)
+        break
+    while Input == varo:
+        new = chr(varO)
+        break
+    while Input == varp:
+        new = chr(varP)
+        break
+    while Input == varq:
+        new = chr(varQ)
+        break
+    while Input == varr:
+        new = chr(varR)
+        break
+    while Input == varst:
+        new = chr(varS)
+        break
+    while Input == vart:
+        new = chr(varT)
+        break
+    while Input == varu:
+        new = chr(varU)
+        break
+    while Input == varv:
+        new = chr(varV)
+        break
+    while Input == varw:
+        new = chr(varW)
+        break
+    while Input == varx:
+        new = chr(varX)
+        break
+    while Input == vary:
+        new = chr(varY)
+        break
+    while Input == varz:
+        new = chr(varZ)
+        break
+print(new)
+        
